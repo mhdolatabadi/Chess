ee1c7c3c4d2644c75331c97b00ab3a50fb29002a
MohammadHossein Dolatabadi <m.h.dolatabadi.a@gmail.com>
2020-06-06 18:27:23 +0430

    complete javafx

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,369 @@
 package sample;
 
+
+
+import com.sun.org.apache.bcel.internal.generic.Select;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+
+import java.awt.*;
+
 public class Controller {
+    public int toX;
+    public int toY;
+    public ImageView WhiteB1;
+    public ImageView Flour1;
+    public ImageView Flour2;
+    public ImageView Flour3;
+    public ImageView Flour4;
+    public ImageView Flour5;
+    public ImageView Flour6;
+    public ImageView Flour7;
+    public ImageView Flour8;
+    public ImageView Flour9;
+    public ImageView Flour10;
+    public ImageView Flour11;
+    public ImageView Flour12;
+    public ImageView Flour13;
+    public ImageView Flour14;
+    public ImageView Flour15;
+    public ImageView Flour16;
+    public ImageView Flour17;
+    public ImageView Flour18;
+    public ImageView Flour19;
+    public ImageView Flour20;
+    public ImageView Flour21;
+    public ImageView Flour22;
+    public ImageView Flour23;
+    public ImageView Flour24;
+    public ImageView Flour25;
+    public ImageView Flour26;
+    public ImageView Flour27;
+    public ImageView Flour28;
+    public ImageView Flour29;
+    public ImageView Flour30;
+    public ImageView Flour31;
+    public ImageView Flour32;
+    public ImageView Flour33;
+    public ImageView Flour34;
+    public ImageView Flour35;
+    public ImageView Flour36;
+    public ImageView Flour37;
+    public ImageView Flour38;
+    public ImageView Flour39;
+    public ImageView Flour40;
+    public ImageView Flour41;
+    public ImageView Flour42;
+    public ImageView Flour43;
+    public ImageView Flour44;
+    public ImageView Flour45;
+    public ImageView Flour46;
+    public ImageView Flour47;
+    public ImageView Flour48;
+    public ImageView Flour49;
+    public ImageView Flour50;
+    public ImageView Flour51;
+    public ImageView Flour52;
+    public ImageView Flour53;
+    public ImageView Flour54;
+    public ImageView Flour55;
+    public ImageView Flour56;
+    public ImageView Flour57;
+    public ImageView Flour58;
+    public ImageView Flour59;
+    public ImageView Flour60;
+    public ImageView Flour61;
+    public ImageView Flour62;
+    public ImageView Flour63;
+    public ImageView Flour64;
+
+    public ImageView[] Flours = new ImageView[64];
+
+    public void flour1Change(){
+        toX = GridPane.getColumnIndex(Flour1);
+        toY = GridPane.getRowIndex(Flour1);
+    }
+
+    public void flour2Change(){
+        toX = GridPane.getColumnIndex(Flour2);
+        toY = GridPane.getRowIndex(Flour2);
+    }
+
+    public void flour3Change(){
+        toX = GridPane.getColumnIndex(Flour3);
+        toY = GridPane.getRowIndex(Flour3);
+    }
+    public void flour4Change(){
+        toX = GridPane.getColumnIndex(Flour4);
+        toY = GridPane.getRowIndex(Flour4);
+    }
+    public void flour5Change(){
+        toX = GridPane.getColumnIndex(Flour5);
+        toY = GridPane.getRowIndex(Flour5);
+    }
+    public void flour6Change(){
+        toX = GridPane.getColumnIndex(Flour6);
+        toY = GridPane.getRowIndex(Flour6);
+    }
+    public void flour7Change(){
+        toX = GridPane.getColumnIndex(Flour7);
+        toY = GridPane.getRowIndex(Flour7);
+    }
+    public void flour8Change(){
+        toX = GridPane.getColumnIndex(Flour8);
+        toY = GridPane.getRowIndex(Flour8);
+    }
+    public void flour9Change(){
+        toX = GridPane.getColumnIndex(Flour9);
+        toY = GridPane.getRowIndex(Flour9);
+    }
+    public void flour10Change(){
+        toX = GridPane.getColumnIndex(Flour10);
+        toY = GridPane.getRowIndex(Flour10);
+    }
+
+    public void flour11Change(){
+        toX = GridPane.getColumnIndex(Flour11);
+        toY = GridPane.getRowIndex(Flour11);
+    }
+
+    public void flour12Change(){
+        toX = GridPane.getColumnIndex(Flour12);
+        toY = GridPane.getRowIndex(Flour12);
+    }
+
+    public void flour13Change(){
+        toX = GridPane.getColumnIndex(Flour13);
+        toY = GridPane.getRowIndex(Flour13);
+    }
+    public void flour14Change(){
+        toX = GridPane.getColumnIndex(Flour14);
+        toY = GridPane.getRowIndex(Flour14);
+    }
+    public void flour15Change(){
+        toX = GridPane.getColumnIndex(Flour15);
+        toY = GridPane.getRowIndex(Flour15);
+    }
+    public void flour16Change(){
+        toX = GridPane.getColumnIndex(Flour16);
+        toY = GridPane.getRowIndex(Flour16);
+    }
+    public void flour17Change(){
+        toX = GridPane.getColumnIndex(Flour17);
+        toY = GridPane.getRowIndex(Flour17);
+    }
+    public void flour18Change(){
+        toX = GridPane.getColumnIndex(Flour18);
+        toY = GridPane.getRowIndex(Flour18);
+    }
+    public void flour19Change(){
+        toX = GridPane.getColumnIndex(Flour19);
+        toY = GridPane.getRowIndex(Flour19);
+    }
+    public void flour20Change(){
+        toX = GridPane.getColumnIndex(Flour20);
+        toY = GridPane.getRowIndex(Flour20);
+    }
+
+    public void flour21Change(){
+        toX = GridPane.getColumnIndex(Flour21);
+        toY = GridPane.getRowIndex(Flour21);
+    }
+
+    public void flour22Change(){
+        toX = GridPane.getColumnIndex(Flour22);
+        toY = GridPane.getRowIndex(Flour22);
+    }
+
+    public void flour23Change(){
+        toX = GridPane.getColumnIndex(Flour23);
+        toY = GridPane.getRowIndex(Flour23);
+    }
+    public void flour24Change(){
+        toX = GridPane.getColumnIndex(Flour24);
+        toY = GridPane.getRowIndex(Flour24);
+    }
+    public void flour25Change(){
+        toX = GridPane.getColumnIndex(Flour25);
+        toY = GridPane.getRowIndex(Flour25);
+    }
+    public void flour26Change(){
+        toX = GridPane.getColumnIndex(Flour26);
+        toY = GridPane.getRowIndex(Flour26);
+    }
+    public void flour27Change(){
+        toX = GridPane.getColumnIndex(Flour27);
+        toY = GridPane.getRowIndex(Flour27);
+    }
+    public void flour28Change(){
+        toX = GridPane.getColumnIndex(Flour28);
+        toY = GridPane.getRowIndex(Flour28);
+    }
+    public void flour29Change(){
+        toX = GridPane.getColumnIndex(Flour29);
+        toY = GridPane.getRowIndex(Flour29);
+    }
+
+    public void flour30Change(){
+        toX = GridPane.getColumnIndex(Flour30);
+        toY = GridPane.getRowIndex(Flour30);
+    }
+
+    public void flour31Change(){
+        toX = GridPane.getColumnIndex(Flour31);
+        toY = GridPane.getRowIndex(Flour31);
+    }
+
+    public void flour32Change(){
+        toX = GridPane.getColumnIndex(Flour32);
+        toY = GridPane.getRowIndex(Flour32);
+    }
+
+    public void flour33Change(){
+        toX = GridPane.getColumnIndex(Flour33);
+        toY = GridPane.getRowIndex(Flour33);
+    }
+    public void flour34Change(){
+        toX = GridPane.getColumnIndex(Flour4);
+        toY = GridPane.getRowIndex(Flour4);
+    }
+    public void flour35Change(){
+        toX = GridPane.getColumnIndex(Flour5);
+        toY = GridPane.getRowIndex(Flour5);
+    }
+    public void flour36Change(){
+        toX = GridPane.getColumnIndex(Flour6);
+        toY = GridPane.getRowIndex(Flour6);
+    }
+    public void flour37Change(){
+        toX = GridPane.getColumnIndex(Flour7);
+        toY = GridPane.getRowIndex(Flour7);
+    }
+    public void flour38Change(){
+        toX = GridPane.getColumnIndex(Flour8);
+        toY = GridPane.getRowIndex(Flour8);
+    }
+    public void flour39Change(){
+        toX = GridPane.getColumnIndex(Flour9);
+        toY = GridPane.getRowIndex(Flour9);
+    }
+
+    public void flour40Change(){
+        toX = GridPane.getColumnIndex(Flour9);
+        toY = GridPane.getRowIndex(Flour9);
+    }
+    public void flour41Change(){
+        toX = GridPane.getColumnIndex(Flour41);
+        toY = GridPane.getRowIndex(Flour41);
+    }
+
+    public void flour42Change(){
+        toX = GridPane.getColumnIndex(Flour42);
+        toY = GridPane.getRowIndex(Flour42);
+    }
+
+    public void flour43Change(){
+        toX = GridPane.getColumnIndex(Flour43);
+        toY = GridPane.getRowIndex(Flour43);
+    }
+    public void flour44Change(){
+        toX = GridPane.getColumnIndex(Flour44);
+        toY = GridPane.getRowIndex(Flour44);
+    }
+    public void flour45Change(){
+        toX = GridPane.getColumnIndex(Flour45);
+        toY = GridPane.getRowIndex(Flour45);
+    }
+    public void flour46Change(){
+        toX = GridPane.getColumnIndex(Flour46);
+        toY = GridPane.getRowIndex(Flour46);
+    }
+    public void flour47Change(){
+        toX = GridPane.getColumnIndex(Flour47);
+        toY = GridPane.getRowIndex(Flour47);
+    }
+    public void flour48Change(){
+        toX = GridPane.getColumnIndex(Flour48);
+        toY = GridPane.getRowIndex(Flour48);
+    }
+    public void flour49Change(){
+        toX = GridPane.getColumnIndex(Flour49);
+        toY = GridPane.getRowIndex(Flour49);
+    }
+    public void flour50Change(){
+        toX = GridPane.getColumnIndex(Flour50);
+        toY = GridPane.getRowIndex(Flour50);
+    }
+    public void flour51Change(){
+        toX = GridPane.getColumnIndex(Flour1);
+        toY = GridPane.getRowIndex(Flour1);
+    }
+
+    public void flour52Change(){
+        toX = GridPane.getColumnIndex(Flour2);
+        toY = GridPane.getRowIndex(Flour2);
+    }
+
+    public void flour53Change(){
+        toX = GridPane.getColumnIndex(Flour3);
+        toY = GridPane.getRowIndex(Flour3);
+    }
+    public void flour54Change(){
+        toX = GridPane.getColumnIndex(Flour4);
+        toY = GridPane.getRowIndex(Flour4);
+    }
+    public void flour55Change(){
+        toX = GridPane.getColumnIndex(Flour5);
+        toY = GridPane.getRowIndex(Flour5);
+    }
+    public void flour56Change(){
+        toX = GridPane.getColumnIndex(Flour6);
+        toY = GridPane.getRowIndex(Flour6);
+    }
+    public void flour57Change(){
+        toX = GridPane.getColumnIndex(Flour7);
+        toY = GridPane.getRowIndex(Flour7);
+    }
+    public void flour58Change(){
+        toX = GridPane.getColumnIndex(Flour8);
+        toY = GridPane.getRowIndex(Flour8);
+    }
+    public void flour59Change(){
+        toX = GridPane.getColumnIndex(Flour9);
+        toY = GridPane.getRowIndex(Flour9);
+    }
+    public void flour60Change(){
+        toX = GridPane.getColumnIndex(Flour59);
+        toY = GridPane.getRowIndex(Flour59);
+    }
+
+    public void flour61Change(){
+        toX = GridPane.getColumnIndex(Flour61);
+        toY = GridPane.getRowIndex(Flour61);
+    }
+
+    public void flour62Change(){
+        toX = GridPane.getColumnIndex(Flour62);
+        toY = GridPane.getRowIndex(Flour62);
+    }
+
+    public void flour63Change(){
+        toX = GridPane.getColumnIndex(Flour63);
+        toY = GridPane.getRowIndex(Flour63);
+    }
+    public void flour64Change(){
+        toX = GridPane.getColumnIndex(Flour64);
+        toY = GridPane.getRowIndex(Flour64);
+    }
+    public void pieceMove(MouseEvent mouseEvent) {
+        GridPane.setColumnIndex(WhiteB1, toX);
+        GridPane.setRowIndex(WhiteB1, toY);
+    }
+
+    public void clickStackPane(StackPane stackPane) {
+        int x = 9 - GridPane.getRowIndex(stackPane);
+        int y = GridPane.getColumnIndex(stackPane);
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -22,32 +22,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Image blackElephant = new Image(new FileInputStream("e:\\Users\\asus\\Documents\\As A Student\\Term 2\\Advance Programming\\Chess\\Resources\\bb.png"));
-
-        ImageView blackElephantView = new ImageView(blackElephant);
-        Menu menu = new Menu("Menu 2");
-        MenuBar menuBar = new MenuBar();
-//        menu.setGraphic(new ImageView("/wn.png"));
-
-        menu.setOnShowing(e -> { System.out.println("Showing Menu 1"); });
-        menu.setOnShown  (e -> { System.out.println("Shown Menu 1"); });
-        menu.setOnHiding (e -> { System.out.println("Hiding Menu 1"); });
-        menu.setOnHidden (e -> { System.out.println("Hidden Menu 1"); });
-        MenuItem menuItem1 = new MenuItem("Item 1");
-        MenuItem menuItem2 = new MenuItem("Item 2");
-        menu.getItems().add(menuItem1);
-        menu.getItems().add(menuItem2);
-        menuBar.getMenus().add(menu);
-        VBox vBox = new VBox(menuBar);
-
-
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        Group game = new Group(blackElephantView, vBox);
-        Scene gameScene = new Scene(game, 800, 800);
-        primaryStage.setTitle("Hello World");
+        Scene gameScene = new Scene(root, 900, 900);
+        primaryStage.setTitle("Pro Chess");
         primaryStage.setScene(gameScene);
         primaryStage.show();
     }
+
 
 
     public static void main(String[] args) {
